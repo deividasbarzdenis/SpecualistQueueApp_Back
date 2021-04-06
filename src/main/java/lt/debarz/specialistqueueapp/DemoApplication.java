d8808cbe91937ec92d33b2e8ee511fb6f85ab634
deividasbardzenis <deividasbarzdenis@yahoo.com>
2021-04-06 23:15:17 +0200

    App is Running and ready for user sing up  and login implementation in Front and

--- src/main/java/lt/debarz/specialistqueueapp/DemoApplication.java
+++ src/main/java/lt/debarz/specialistqueueapp/DemoApplication.java
@@ -2,8 +2,10 @@ package lt.debarz.specialistqueueapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+//(exclude = { SecurityAutoConfiguration.class })
 public class DemoApplication {
 
     public static void main(String[] args) {
