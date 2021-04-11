6e073c605d17ff9913b6a78735364ffccb127dbf
deividasbardzenis <deividasbarzdenis@yahoo.com>
2021-04-11 11:28:09 +0200

    Create Queue entity wit field required for client register to specialist. Queue objects will be save to DB.

---
+++ src/main/java/lt/debarz/specialistqueueapp/queue/model/Queue.java
@@ -0,0 +1,48 @@
+package lt.debarz.specialistqueueapp.queue.model;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Queue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank
+    private Integer queueNumber;
+
+    @Column(nullable = false)
+    @NotBlank
+    @Size(min=3, max=254)
+    private String name;
+
+    @Column(nullable = false)
+    @NotBlank
+    @Size(min=3, max=254)
+    private String lastname;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date creationTime = new Date();
+
+    //it need to be calculated then mapping dto to entity
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date registerTime;
+
+
+
+}
